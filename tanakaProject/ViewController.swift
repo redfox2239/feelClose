982839c995afd12c78257e7983176b879e93f0e9
原田礼朗 <rharada@mba-11aj-090.local>
2016-06-25 18:12:09 +0900

    Initial Commit

---
+++ tanakaProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tanakaProject
+//
+//  Created by 原田　礼朗 on 2016/06/25.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
